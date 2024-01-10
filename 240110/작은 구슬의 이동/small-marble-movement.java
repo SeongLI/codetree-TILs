0e3f8204e62847a10ca292cd293d29f4015cf38e
이은성 <110223414+SeongLI@users.noreply.github.com>
2024-01-10 13:08:46 +0900

    Title: 작은 구슬의 이동, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240110/작은 구슬의 이동/small-marble-movement.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // 격자의 크기
+        int t = sc.nextInt(); // 시간
+        int[][] map = new int[n][n];
+        
+        int row = sc.nextInt() - 1;
+        int col = sc.nextInt() - 1;
+        // 상하우좌
+        int[] dr = {1, -1, 0, 0};
+        int[] dc = {0, 0, 1, -1};
+
+        String direction = sc.next();
+        int dir =-1;
+
+        if(direction.equals("U")){
+            dir = 0;
+        } else if(direction.equals("D")){
+            dir = 1;
+        } else if(direction.equals("R")){
+            dir = 2;
+        } else {
+            dir = 3;
+        }
+
+        while(t > 0){
+            int nr = row + dr[dir];
+            int nc = col + dc[dir];
+            if(nr < 0 || nc < 0 || nr >= n || nc >= n){
+                if(dir == 0) dir = 1;
+                else if(dir == 1) dir = 0;
+                else if(dir == 2) dir = 3;
+                else dir = 2;
+                t--;
+                continue;
+            }
+            row = nr;
+            col = nc;
+            t--;
+        }
+        row ++;
+        col ++;
+
+        System.out.print(row +" "+col);
+
+
+
+    }
+}
