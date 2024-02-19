c98137d300a5390730d1ef6591009c1bd6cad533
이은성 <110223414+SeongLI@users.noreply.github.com>
2024-02-19 17:36:20 +0900

    Title: 바이러스 검사, Time: 3091ms, Memory: 155MB, Status: Time Limit Exceed - CodeTree

---
+++ 240219/바이러스 검사/virus-detector.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = sc.nextInt();
+        }
+        int sup = sc.nextInt();
+        int sub = sc.nextInt();
+
+        int cnt = 0;
+        for(int i = 0 ; i < N ; i++){
+            int temp = arr[i];
+            temp -= sup;
+            cnt++;
+            while(temp > 0){
+                temp-= sub;
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
